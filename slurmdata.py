8f87262649ee9959f7ab2e07380c1317ae0e9c93
Roy Einar Dragseth <roy.dragseth@uit.no>
2016-10-06 20:29:45 +0200

    Added node info.

--- slurmdata.py
+++ slurmdata.py
@@ -26,5 +26,22 @@ def get_squeue_data():
 def returnsqueue():
     return {'jobs' : get_squeue_data()}
 
+def get_sinfo_data():
+    queuedata = StringIO.StringIO(os.popen("sinfo -o %all", 'r').read())
+    reader = csv.reader(queuedata, delimiter='|')
+    nodelist = set()
+    headers = map(convert, reader.next())
+    hostnameIdx = headers.index("HOSTNAMES")
+    rows = list()
+    for row in reader:
+        if not row[hostnameIdx] in nodelist:
+            rows.append(map(convert, row))
+            nodelist.add(row[hostnameIdx])
+    return {'headers' : headers, 'nodes' : rows }
+
+@route('/data/sinfo')
+def returnsqueue():
+    return get_sinfo_data()
+
 
 run(host='localhost', port=8080, debug=True, reloader=True)
