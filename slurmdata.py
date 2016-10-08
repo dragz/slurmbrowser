fca8e59f1844a5b017f8769a5f1fcd8d04d59e2b
Roy Einar Dragseth <roy.dragseth@uit.no>
2016-10-08 18:24:41 +0200

    Split headers and jobs. Added expanded hostlist

--- slurmdata.py
+++ slurmdata.py
@@ -2,6 +2,7 @@ import os
 import csv
 import StringIO
 from bottle import route, run, static_file
+from hostlist import expand_hostlist
 
 @route('/<filename>')
 def server_static(filename):
@@ -11,7 +12,6 @@ def convert(a):
     b = a.strip()
     if b.isdigit():
         return int(b)
-
     try:
         return float(b)
     except ValueError:
@@ -20,11 +20,20 @@ def convert(a):
 def get_squeue_data():
     queuedata = StringIO.StringIO(os.popen("squeue -o %all", 'r').read())
     reader = csv.reader(queuedata, delimiter='|')
-    return [map(convert, row) for row in reader]
+    headers = map(convert, reader.next())
+    hl_idx = headers.index("NODELIST(REASON)")
+    headers.append("FULL_NODELIST")
+    rows = list()
+    for row in reader:
+        full_hostlist = map(str.strip, expand_hostlist(row[hl_idx]))
+        row.append(str(',').join(full_hostlist))
+        rows.append(map(convert, row))
+
+    return {'headers' : headers, 'jobs': rows}
 
 @route('/data/squeue')
 def returnsqueue():
-    return {'jobs' : get_squeue_data()}
+    return get_squeue_data()
 
 def get_sinfo_data():
     queuedata = StringIO.StringIO(os.popen("sinfo -o %all", 'r').read())
