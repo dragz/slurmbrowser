f4ddc57ef2e2d901e6bbabc7e83f1d31c25f5477
Roy Dragseth <roy.dragseth@uit.no>
2019-08-01 09:42:54 +0200

    Prettified slurmdata.py using autopep8.

--- slurmdata.py
+++ slurmdata.py
@@ -1,3 +1,5 @@
+from hostlist import expand_hostlist
+from bottle import route, run, static_file, request, response, default_app
 import os
 import sys
 import re
@@ -5,8 +7,12 @@ import csv
 import io
 import time
 import datetime as dt
-import urllib.request, urllib.parse, urllib.error
-import urllib.request, urllib.error, urllib.parse
+import urllib.request
+import urllib.parse
+import urllib.error
+import urllib.request
+import urllib.error
+import urllib.parse
 from urllib.request import urlopen
 from pipes import quote
 from configparser import ConfigParser
@@ -15,8 +21,6 @@ from configparser import ConfigParser
 if not __name__ == "__main__":
     sys.path.append(os.path.dirname(__file__))
     os.chdir(os.path.dirname(__file__))
-from bottle import route, run, static_file, request, response, default_app
-from hostlist import expand_hostlist
 
 GANGLIA = 1
 GANGLIA_PROC = 1
@@ -33,8 +37,8 @@ if GANGLIA:
 
     if user:
         auth_handler = urllib.request.HTTPBasicAuthHandler()
-        auth_handler.add_password(realm = "Ganglia web UI", uri = serverurl,
-                                  user = user, passwd = passwd)
+        auth_handler.add_password(realm="Ganglia web UI", uri=serverurl,
+                                  user=user, passwd=passwd)
         opener = urllib.request.build_opener(auth_handler)
         urllib.request.install_opener(opener)
 
@@ -48,18 +52,21 @@ if GANGLIA:
 
 #
 # This section of code is only used when producing screencasts
-# all it does is hide the real usernames 
+# all it does is hide the real usernames
 #
-OBFUSCATED_USERNAMES=0
+OBFUSCATED_USERNAMES = 0
+
 
 def obfuscated_usernames():
-    usernames = os.popen("squeue -h -o %u","r").readlines()
+    usernames = os.popen("squeue -h -o %u", "r").readlines()
     uu = set(map(str.strip, usernames))
     a = ord('a')
     z = ord('z')
-    ou = [ chr(s)+chr(t)+"user" for s in range(ord('a'), ord('z')+1) for t in range(ord('a'), ord('z')+1)]
+    ou = [chr(s)+chr(t)+"user" for s in range(ord('a'), ord('z')+1)
+          for t in range(ord('a'), ord('z')+1)]
     usermap = list(zip(uu, ou))
     return usermap
+
 
 def hide_usernames(t):
     """hide usernames when producing images or videos"""
@@ -67,8 +74,9 @@ def hide_usernames(t):
     if OBFUSCATED_USERNAMES:
         um = obfuscated_usernames()
         for u, o in um:
-            t = t.replace(u,o)
+            t = t.replace(u, o)
     return t
+
 
 def reverse_hidden_user(user):
     """reverse lookup hidden usernames when producing images or videos"""
@@ -83,9 +91,11 @@ def reverse_hidden_user(user):
 #
 #
 
+
 @route('/html/<filename>')
 def server_static(filename):
     return static_file(filename, root='.')
+
 
 def convert(a):
     b = a.strip()
@@ -95,6 +105,7 @@ def convert(a):
         return float(b)
     except ValueError:
         return b
+
 
 def get_squeue_data():
     t0 = time.time()
@@ -112,11 +123,13 @@ def get_squeue_data():
         rows.append(list(map(convert, row)))
     print("total squeue", time.time() - t0)
 
-    return {'headers' : headers, 'jobs': rows}
+    return {'headers': headers, 'jobs': rows}
+
 
 @route('/data/squeue')
 def returnsqueue():
     return get_squeue_data()
+
 
 def get_sinfo_data():
     t0 = time.time()
@@ -131,16 +144,18 @@ def get_sinfo_data():
             rows.append(list(map(convert, row)))
             nodelist.add(row[hostnameIdx])
     print("sinfo", time.time() - t0)
-    return {'headers' : headers, 'nodes' : rows }
+    return {'headers': headers, 'nodes': rows}
+
 
 @route('/data/sinfo')
 def returnsinfo():
     return get_sinfo_data()
 
+
 @route('/data/nodeinfo')
 def returnnodeinfo(nodelist=""):
     t0 = time.time()
-    cmd = "scontrol show -d --oneliner node " 
+    cmd = "scontrol show -d --oneliner node "
     if nodelist:
         cmd += quote(nodelist)
     print(cmd)
@@ -151,12 +166,12 @@ def returnnodeinfo(nodelist=""):
     # regex101.com to the rescue
     # matches word=anything not containing word=
     parsenodeinfo = re.compile(r'(\w+)=(.*?)(?:(?= +\w+?=)|$)')
-    conv = lambda t: (t[0], convert(t[1]))
+    def conv(t): return (t[0], convert(t[1]))
     for l in s:
-      nodedata = dict(map(conv, parsenodeinfo.findall(l)))
-      nodeinfo.append(nodedata)
+        nodedata = dict(map(conv, parsenodeinfo.findall(l)))
+        nodeinfo.append(nodedata)
     print("nodeinfo ", time.time() - t0)
-    return {'nodeinfo' : nodeinfo}
+    return {'nodeinfo': nodeinfo}
 
 
 def get_procs(nodelist):
@@ -168,54 +183,57 @@ def get_procs(nodelist):
     psinfo = Metrics()
     for node in nodelist:
         host_filter = Filter(hl=[node]).hostlist
-        s = socket.create_connection((host,port))
-        s.sendall(("/frontends/%s/\n" % node).encode('utf-8'))         # beware, if the nodename isn't recognized gmetad will dump the whole database on you.
-        fileobject = s.makefile('b',encoding='utf-8')
+        s = socket.create_connection((host, port))
+        # beware, if the nodename isn't recognized gmetad will dump the whole database on you.
+        s.sendall(("/frontends/%s/\n" % node).encode('utf-8'))
+        fileobject = s.makefile('b', encoding='utf-8')
         node_psinfo = Metrics(infile=fileobject, filter=Filter("ps-").startswith,
-                        host_filter=host_filter) 
+                              host_filter=host_filter)
         print(node_psinfo)
         psinfo.update(node_psinfo)
     return list(psinfo.items())
-
 
 
 @route('/data/job/<jobid:re:\d+_?\[?\d*\]?>')
 def returnjobinfo(jobid):
     global GANGLIA, GANGLIA_PROC
     t0 = time.time()
-    #print jobid
-    s = hide_usernames(os.popen("scontrol show -d --oneliner job " + str(jobid)).read()).split()
+    # print jobid
+    s = hide_usernames(
+        os.popen("scontrol show -d --oneliner job " + str(jobid)).read()).split()
     print("slurm response took ", time.time() - t0)
     if s:
-      j = dict()
-      for x in s:
-        y = x.split('=', 1)
-        if len(y) == 2:
-          j[y[0]] = y[1]
-        
-      cpu_mapping = list()
-      h = ['Nodes', 'CPU_IDs', 'Mem']
-      nodelist = ""
-      for i, n in enumerate(s):
-        if n.startswith("Nodes="):
-          cpu_mapping.append([s[i].replace('Nodes=',''),
-                             s[i+1].replace('CPU_IDs=',''),
-                             s[i+2].replace('Mem=','')])
-        if n.startswith("NodeList="):
-          nodelist = n.replace("NodeList=", "")
-      j['cpu_mapping'] = {'headers' : h, 'nodes' : cpu_mapping}
-      j['expanded_nodelist'] = list(map(str.strip, expand_hostlist(nodelist)))
-      if 'Nodes' in j:
-        j['nodeinfo'] = returnnodeinfo(j['Nodes'])['nodeinfo']
-      if GANGLIA == 1 and GANGLIA_PROC == 1:
-          t_procs0 = time.time()
-          j['procs'] = get_procs(j['expanded_nodelist'])
-          print("get procs took", time.time() - t_procs0)
+        j = dict()
+        for x in s:
+            y = x.split('=', 1)
+            if len(y) == 2:
+                j[y[0]] = y[1]
+
+        cpu_mapping = list()
+        h = ['Nodes', 'CPU_IDs', 'Mem']
+        nodelist = ""
+        for i, n in enumerate(s):
+            if n.startswith("Nodes="):
+                cpu_mapping.append([s[i].replace('Nodes=', ''),
+                                    s[i+1].replace('CPU_IDs=', ''),
+                                    s[i+2].replace('Mem=', '')])
+            if n.startswith("NodeList="):
+                nodelist = n.replace("NodeList=", "")
+        j['cpu_mapping'] = {'headers': h, 'nodes': cpu_mapping}
+        j['expanded_nodelist'] = list(
+            map(str.strip, expand_hostlist(nodelist)))
+        if 'Nodes' in j:
+            j['nodeinfo'] = returnnodeinfo(j['Nodes'])['nodeinfo']
+        if GANGLIA == 1 and GANGLIA_PROC == 1:
+            t_procs0 = time.time()
+            j['procs'] = get_procs(j['expanded_nodelist'])
+            print("get procs took", time.time() - t_procs0)
     else:
-        #not an active job, fetch finished job stats
-        #remark, these stats have a different format, leave it up to the client side
-        #to fix it.
-        yesterday = (dt.datetime.today() - dt.timedelta(1)).strftime("%Y-%m-%d")
+            # not an active job, fetch finished job stats
+            # remark, these stats have a different format, leave it up to the client side
+            # to fix it.
+        yesterday = (dt.datetime.today() -
+                     dt.timedelta(1)).strftime("%Y-%m-%d")
         sacct = "sacct -X --format=jobid,jobname,user,account,state,elapsed,submit,start,end,nnodes,ncpus,reqnodes,reqcpus,nodelist --parsable2 -S %s --job %s"
         s = hide_usernames(os.popen(sacct % (yesterday, jobid), 'r').read())
         t = io.StringIO(s)
@@ -223,18 +241,21 @@ def returnjobinfo(jobid):
         headers = list(map(convert, next(reader)))
         jobinfo = list(map(convert, next(reader)))
         j = dict(list(zip(headers, jobinfo)))
-        j['expanded_nodelist'] = list(map(str.strip, expand_hostlist(j["NodeList"])))
+        j['expanded_nodelist'] = list(
+            map(str.strip, expand_hostlist(j["NodeList"])))
     j['GANGLIA'] = GANGLIA
-    #print j
+    # print j
     print("jobinfo", time.time() - t0)
     return j
+
 
 @route('/data/jobhist/<user>')
 def returnjobhist(user):
     user = reverse_hidden_user(user)
     t0 = time.time()
     yesterday = (dt.datetime.today() - dt.timedelta(1)).strftime("%Y-%m-%d")
-    sacct = "sacct -S %s -X --format=jobid,jobname,user,account,state,elapsed,start,end,nnodes,ncpus,nodelist --parsable2 -u %s" % (yesterday, quote(user))
+    sacct = "sacct -S %s -X --format=jobid,jobname,user,account,state,elapsed,start,end,nnodes,ncpus,nodelist --parsable2 -u %s" % (
+        yesterday, quote(user))
     s = hide_usernames(os.popen(sacct, 'r').read())
     t = io.StringIO(s)
     reader = csv.reader(t, delimiter='|')
@@ -253,14 +274,17 @@ def fetchgraph():
     global graphurl
     try:
         hostgraphurl = (graphurl.replace('GRAPH_NAME', request.query.name)
-                           .replace('STARTTIME',  request.query.start)
-                           .replace('ENDTIME',    request.query.end)
-                  )
+                        .replace('STARTTIME',  request.query.start)
+                        .replace('ENDTIME',    request.query.end)
+                        )
         if request.query.hostname:
-            hostgraphurl = hostgraphurl.replace('HOSTNAME', request.query.hostname)
+            hostgraphurl = hostgraphurl.replace(
+                'HOSTNAME', request.query.hostname)
         if request.query.hl:
             hostgraphurl += "&glegend=hide&gtype=line&z=large&line_width=0"
-            hostgraphurl += "&aggregate=1&hreg[]=" + urllib.parse.quote("|".join([ h + "\\b" for h in request.query.hl.split(',')]))
+            hostgraphurl += "&aggregate=1&hreg[]=" + \
+                urllib.parse.quote(
+                    "|".join([h + "\\b" for h in request.query.hl.split(',')]))
             hostgraphurl += "&mreg[]=^" + request.query.mreg
             hostgraphurl += "&title=" + request.query.mreg
         print(hostgraphurl)
@@ -271,9 +295,8 @@ def fetchgraph():
         graph = None
     return graph
 
+
 if __name__ == "__main__":
     run(host='localhost', port=9080, debug=True, reloader=True)
 else:
     application = default_app()
-
-
